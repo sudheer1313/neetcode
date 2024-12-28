93d1dd861a52682504c82730041445692402b717
sudheer1313 <sudheer.somepalli@gmail.com>
2024-12-28 16:04:43 -0500

    Update duplicate.py with new solution

--- duplicate.py
+++ duplicate.py
@@ -1,10 +1,7 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        s = set()
-        for i in nums:
-            if i in s:
-                return True
-            else:
-                s.add(i)
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+                    return True
         return False
-
