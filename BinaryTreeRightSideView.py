e5f9d5681ee8320a1e2244b181e5a63604627841
sudheer1313 <sudheer.somepalli@gmail.com>
2025-01-05 23:08:55 -0500

    BinaryTreeRightSideView.py

---
+++ BinaryTreeRightSideView.py
@@ -0,0 +1,30 @@
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        q = deque([root])
+        res = []
+        while q:
+            l = len(q)
+            for i in range(l):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if i == l - 1:
+                    res.append(node.val)
+        return res
+
+
+
+
