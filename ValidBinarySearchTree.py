96ed13d3b64c615a546ce58dabc1c00385cc421a
sudheer1313 <sudheer.somepalli@gmail.com>
2025-01-05 23:01:46 -0500

    ValidBinarySearchTree.py

---
+++ ValidBinarySearchTree.py
@@ -0,0 +1,24 @@
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res == sorted(res) and len(res) == len(set(res))
+
