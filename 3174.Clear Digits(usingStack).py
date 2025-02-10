2dedb4fb656857c17b4e5886d47a1237c9e682c5
sudheer1313 <sudheer.somepalli@gmail.com>
2025-02-10 13:21:26 -0500

    3174.Clear Digits(usingStack).py

---
+++ 3174.Clear Digits(usingStack).py
@@ -0,0 +1,20 @@
+
+
+
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for i in s:
+            if i.isdigit():
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)
+
+        # time complexity:o(n)
+        # space complexity:o(n)
+
+
+
+
