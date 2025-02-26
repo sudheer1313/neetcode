c5fab4d857ac4d814f893407ef6d2cfd5ed08306
sudheer1313 <sudheer.somepalli@gmail.com>
2025-02-25 20:20:13 -0500

    1524.NumberofSub-arraysWithOddSum(bruteforce).py

---
+++ 1524.NumberofSub-arraysWithOddSum(bruteforce).py
@@ -0,0 +1,13 @@
+1524.NumberofSub-arraysWithOddSum(bruteforce).py
+
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        c = 0
+        for i in range(len(arr)):
+            s = 0
+            for j in range(i, len(arr)):
+                s += arr[j]
+                if s % 2 != 0:
+                    c += 1
+        return c
